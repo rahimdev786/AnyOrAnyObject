007aaf19c348765964e62588f3546ed96cb7f00e
rahim <rahimdev786@gmail.com>
2022-04-25 18:40:10 +0530

    comit

--- AnyOrAnyObject/ViewController.swift
+++ AnyOrAnyObject/ViewController.swift
@@ -5,6 +5,18 @@
 //  Created by arshad on 4/25/22.
 //
 
+
+
+/*
+ 
+ any : represent any type  --> let  -> struct
+ anyObject : represent only class  --> var  --> class
+ 
+ to retrcet protocal use for struct and enum use anyobject. so that protcal used only for class
+ 
+ 
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +24,63 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let data:[Any] = [1,23333,90,90.09,"asasassa","as","a",32121.09890]
+        print(data)
+        
+        
+        
+        //so here can not used anyobject
+        //let data1:[AnyObject] = ["sacasc",23221,334.34543]
     }
+}
 
 
+
+class A:ABC{
+    func useData() {
+        print("Hello world")
+    }
+    
+    
+}
+
+
+struct XXX:ABC {
+    func useData() {
+        print("Usful Data")
+    }
+}
+
+
+class B:BData{
+    func firstOne() {
+        print("Khana Baba Sit jy")
+    }
+    
+    func useData() {
+        print("Hello world")
+    }
+    
+    
+}
+
+//
+//struct X:BData {
+//    func useData() {
+//        print("Usful Data")
+//    }
+//}
+
+
+
+protocol BData:AnyObject{
+    func firstOne()
+}
+
+
+protocol ABC:Any{
+    func useData()
 }
 
